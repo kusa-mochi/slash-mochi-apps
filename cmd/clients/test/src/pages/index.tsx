9b48de6ffc7256b3054a8e730f95d0558ee055c2
kusa-mochi <whoatemyapplepie@gmail.com>
2023-10-28 19:10:49 +0900

    [WIP] making client base

--- cmd/clients/test/src/pages/index.tsx
+++ cmd/clients/test/src/pages/index.tsx
@@ -2,10 +2,27 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import React, { useState } from 'react'
+import { createPromiseClient } from '@connectrpc/connect'
+import { createConnectTransport } from '@connectrpc/connect-web'
+import { TestService } from '@/../../../../gen/es/slash_mochi/v1/test/test_connect'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const transport = createConnectTransport({
+    baseUrl: "http://localhost:3081",
+  })
+  const connectClient = createPromiseClient(TestService, transport)
+  const [loopbackResult, setLoopbackResult] = useState("")
+  function DoLoopbackTest(message: string): void {
+    connectClient.loopback({
+      message: message,
+    }).then((res) => {
+      // setLoopbackResult(res.message)
+      console.log(res)
+    })
+  }
   return (
     <>
       <Head>
@@ -20,93 +37,12 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
         </div>
+
+        <div className='loopback-result'>{loopbackResult}</div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          <button onClick={(event) => DoLoopbackTest("hoge-")}>Loopback</button>
         </div>
       </main>
     </>
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	connectrpc.com/connect v1.11.1
 	connectrpc.com/grpcreflect v1.2.0
 	github.com/rs/cors v1.10.1
-	google.golang.org/protobuf v1.31.0
 )
+
+require google.golang.org/protobuf v1.31.0 // indirect
