234db1f914f1653261188e0ccc1924444dc5e7ad
Kusa Mochi <whoatemyapplepie@gmail.com>
2023-11-12 15:10:25 +0900

    make a method to get current method name.

--- cmd/.client-stubs/test_common/test_common.go
+++ cmd/.client-stubs/test_common/test_common.go
@@ -1,5 +1,10 @@
 package test_common
 
+import (
+	"runtime"
+	"strings"
+)
+
 type TestResult struct {
 	TestName    string
 	IsSucceeded bool
@@ -7,4 +12,23 @@ type TestResult struct {
 
 type Tester interface {
 	Test() []TestResult
+}
+
+func GetCurrentMethodName() string {
+	pc, _, _, ok := runtime.Caller(1)
+	if !ok {
+		return ""
+	}
+	f := runtime.FuncForPC(pc)
+	if f == nil {
+		return ""
+	}
+	fullName := f.Name()
+	splittedFullName := strings.Split(fullName, ".")
+	lenSplittedFullName := len(splittedFullName)
+	if lenSplittedFullName == 0 {
+		return ""
+	}
+
+	return splittedFullName[lenSplittedFullName-1]
 }
