f429631eb9c087aadd20fb47f8a6224b539c8297
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-02-18 10:58:06 +0900

    set a client project to build SPA.

--- cmd/clients/flexible-reversi/next.config.mjs
+++ cmd/clients/flexible-reversi/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+const nextConfig = {
+    output:'export',
+    images: {
+        unoptimized: true
+    },
+    assetPrefix: isDevelopment ? '' : '/flexible-reversi/',
+};
 
 export default nextConfig;
