27926c630548835cd0985f8e668e54ccb724ff7e
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-02-18 18:26:13 +0900

    make basic layout to test global chat.

--- cmd/clients/flexible-reversi/app/page.tsx
+++ cmd/clients/flexible-reversi/app/page.tsx
@@ -3,6 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="relative items-center font-mono w-full">
+        <div className="w-full">
+          
+        </div>
+        <div>
+          <input placeholder="input chat message" className="w-1/2"></input><button>Send</button>
+        </div>
+      </div>
+
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
