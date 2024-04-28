9f1b5c6f856ec21a2047e2d4e90a43efa332932e
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-04-28 23:22:09 +0900

    update RPC defs.

--- cmd/server/connect-apis/can-i-use-up-food/can-i-use-up-food.go
+++ cmd/server/connect-apis/can-i-use-up-food/can-i-use-up-food.go
@@ -20,61 +20,89 @@ func NewCanIUseUpFoodService(channels *can_i_use_up_food_store.CanIUseUpFoodStor
 }
 
 // AddFoodToList implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) AddFoodToList(context.Context, *connect.Request[can_i_use_up_foodv1.AddFoodToListRequest]) (*connect.Response[emptypb.Empty], error) {
+func (c *CanIUseUpFoodService) AddFoodToList(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.AddFoodToListRequest]) (*connect.Response[can_i_use_up_foodv1.NewID], error) {
 	panic("unimplemented")
 }
 
 // LoadProjectData implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) LoadProjectData(context.Context, *connect.Request[can_i_use_up_foodv1.LoadProjectDataRequest]) (*connect.Response[can_i_use_up_foodv1.ProjectData], error) {
+func (c *CanIUseUpFoodService) LoadProjectData(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.LoadProjectDataRequest]) (*connect.Response[can_i_use_up_foodv1.ProjectData], error) {
 	panic("unimplemented")
 }
 
 // MakeCalendarItemGroup implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) MakeCalendarItemGroup(context.Context, *connect.Request[can_i_use_up_foodv1.MakeCalendarItemGroupRequest]) (*connect.Response[emptypb.Empty], error) {
+func (c *CanIUseUpFoodService) MakeCalendarItemGroup(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.MakeCalendarItemGroupRequest]) (*connect.Response[can_i_use_up_foodv1.NewID], error) {
 	panic("unimplemented")
 }
 
 // ModifyCalendarPeriod implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) ModifyCalendarPeriod(context.Context, *connect.Request[can_i_use_up_foodv1.ModifyCalendarPeriodRequest]) (*connect.Response[emptypb.Empty], error) {
+func (c *CanIUseUpFoodService) ModifyCalendarPeriod(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.ModifyCalendarPeriodRequest]) (*connect.Response[emptypb.Empty], error) {
 	panic("unimplemented")
 }
 
 // ModifyFoodAmountInCalendar implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) ModifyFoodAmountInCalendar(context.Context, *connect.Request[can_i_use_up_foodv1.ModifyFoodAmountInCalendarRequest]) (*connect.Response[emptypb.Empty], error) {
+func (c *CanIUseUpFoodService) ModifyFoodAmountInCalendar(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.ModifyFoodAmountInCalendarRequest]) (*connect.Response[emptypb.Empty], error) {
 	panic("unimplemented")
 }
 
 // ModifyFoodInList implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) ModifyFoodInList(context.Context, *connect.Request[can_i_use_up_foodv1.ModifyFoodInListRequest]) (*connect.Response[emptypb.Empty], error) {
+func (c *CanIUseUpFoodService) ModifyFoodInList(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.ModifyFoodInListRequest]) (*connect.Response[emptypb.Empty], error) {
 	panic("unimplemented")
 }
 
 // MoveFoodInCalendar implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) MoveFoodInCalendar(context.Context, *connect.Request[can_i_use_up_foodv1.MoveFoodInCalendarRequest]) (*connect.Response[emptypb.Empty], error) {
+func (c *CanIUseUpFoodService) MoveFoodInCalendar(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.MoveFoodInCalendarRequest]) (*connect.Response[emptypb.Empty], error) {
 	panic("unimplemented")
 }
 
 // NewProject implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) NewProject(context.Context, *connect.Request[can_i_use_up_foodv1.NewProjectRequest]) (*connect.Response[can_i_use_up_foodv1.ProjectUri], error) {
+func (c *CanIUseUpFoodService) NewProject(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.NewProjectRequest],
+) (*connect.Response[can_i_use_up_foodv1.ProjectUri], error) {
 	panic("unimplemented")
 }
 
 // PutFoodToCalendar implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) PutFoodToCalendar(context.Context, *connect.Request[can_i_use_up_foodv1.PutFoodToCalendarRequest]) (*connect.Response[emptypb.Empty], error) {
+func (c *CanIUseUpFoodService) PutFoodToCalendar(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.PutFoodToCalendarRequest]) (*connect.Response[can_i_use_up_foodv1.NewID], error) {
 	panic("unimplemented")
 }
 
 // RemoveFoodFromCalendar implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) RemoveFoodFromCalendar(context.Context, *connect.Request[can_i_use_up_foodv1.RemoveFoodFromCalendarRequest]) (*connect.Response[emptypb.Empty], error) {
+func (c *CanIUseUpFoodService) RemoveFoodFromCalendar(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.RemoveFoodFromCalendarRequest]) (*connect.Response[emptypb.Empty], error) {
 	panic("unimplemented")
 }
 
 // RemoveFoodsFromList implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) RemoveFoodsFromList(context.Context, *connect.Request[can_i_use_up_foodv1.RemoveFoodsFromListRequest]) (*connect.Response[emptypb.Empty], error) {
+func (c *CanIUseUpFoodService) RemoveFoodsFromList(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.RemoveFoodsFromListRequest],
+) (*connect.Response[emptypb.Empty], error) {
 	panic("unimplemented")
 }
 
 // UpdateAllData implements can_i_use_up_foodv1connect.CanIUseUpFoodServiceHandler.
-func (c *CanIUseUpFoodService) UpdateAllData(context.Context, *connect.Request[can_i_use_up_foodv1.UpdateAllDataRequest], *connect.ServerStream[can_i_use_up_foodv1.ProjectData]) error {
+func (c *CanIUseUpFoodService) UpdateAllData(
+	ctx context.Context,
+	req *connect.Request[can_i_use_up_foodv1.UpdateAllDataRequest],
+	stream *connect.ServerStream[can_i_use_up_foodv1.ProjectData],
+) error {
 	panic("unimplemented")
 }
