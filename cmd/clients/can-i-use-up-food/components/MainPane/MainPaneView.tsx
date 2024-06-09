0b9639423a8eda065c6c80181dc286e999c97adf
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-06-09 17:53:02 +0900

    making layout base.

--- cmd/clients/can-i-use-up-food/components/MainPane/MainPaneView.tsx
+++ cmd/clients/can-i-use-up-food/components/MainPane/MainPaneView.tsx
@@ -3,12 +3,13 @@ import FoodListPane from "../FoodListPane/FoodListPane";
 
 export default function MainPaneView() {
     return (
-        <>
-            <div>main-pane-view</div>
-            <div>
+        <div className="grow flex flex-row items-stretch justify-start w-full">
+            <div className="basis-80">
                 <FoodListPane></FoodListPane>
+            </div>
+            <div className="grow">
                 <CalendarPane></CalendarPane>
             </div>
-        </>
+        </div>
     )
 }
--- cmd/clients/can-i-use-up-food/components/TitleBar/TitleBarView.tsx
+++ cmd/clients/can-i-use-up-food/components/TitleBar/TitleBarView.tsx
@@ -4,9 +4,9 @@ interface Props {
 
 export default function TitleBarView({title}: Props) {
     return (
-        <>
-            <div>title-bar-view</div>
+        <div className="w-screen h-12 bg-green-600 text-white shadow-md flex flex-row items-center justify-start px-4">
+            <div className="text-2xl mr-4">Can I Use Up Food ?</div>
             <div>{title}</div>
-        </>
+        </div>
     )
 }
