711bcb34975d63fc91a298726a4ff1d3ec65df4c
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-06-15 01:28:39 +0900

    make useContext sample.

--- cmd/clients/can-i-use-up-food/app/page.tsx
+++ cmd/clients/can-i-use-up-food/app/page.tsx
@@ -1,13 +1,19 @@
+"use client"
+
 import TitleBar from "@/components/TitleBar/TitleBar";
 import MainPane from "@/components/MainPane/MainPane";
 import Image from "next/image";
+import { ProjectContext } from "@/context/ProjectContext";
+import { useState } from "react";
 
 export default function Home() {
+  const [test, setTest] = useState("ほげほげ")
   return (
-    <main className="flex flex-col items-center justify-start h-screen">
-      <TitleBar>Project Name</TitleBar>
-      <MainPane></MainPane>
-    </main>
+    <ProjectContext.Provider value={test}>
+      <main className="flex flex-col items-center justify-start h-screen">
+        <TitleBar>Project Name</TitleBar>
+        <MainPane></MainPane>
+      </main>
     </ProjectContext.Provider>
   );
 }
--- cmd/clients/can-i-use-up-food/components/TitleBar/TitleBarView.tsx
+++ cmd/clients/can-i-use-up-food/components/TitleBar/TitleBarView.tsx
@@ -1,12 +1,16 @@
+import { ProjectContext } from "@/context/ProjectContext"
+import { useContext } from "react"
+
 interface Props {
     title: string | undefined
 }
 
 export default function TitleBarView({title}: Props) {
+    const ctx = useContext(ProjectContext)
     return (
         <div className="w-screen h-12 bg-green-800 text-white shadow-md flex flex-row items-center justify-start px-4">
             <div className="text-2xl mr-4">Can I Use Up Food ?</div>
-            <div>{title}</div>
+            <div>{title}{ctx}</div>
         </div>
     )
 }
