3a2e13ff0ff925e2bafc7dae109eda85d30f50f8
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-06-15 21:22:19 +0900

    remove unused context data from views.

--- cmd/clients/can-i-use-up-food/components/FoodListPane/FoodListPaneView.tsx
+++ cmd/clients/can-i-use-up-food/components/FoodListPane/FoodListPaneView.tsx
@@ -2,11 +2,9 @@ import { ProjectContext } from "@/context/ProjectContext"
 import { useContext } from "react"
 
 export default function FoodListPaneView() {
-    const ctx = useContext(ProjectContext)
     return (
         <div className="h-full shadow-md bg-green-50">
             <div>foodlist-pane-view</div>
-            <div>{ctx.testName}|{ctx.testNumber}|{ctx.testChild.testChildName}</div>
         </div>
     )
 }
--- cmd/clients/can-i-use-up-food/components/TitleBar/TitleBarView.tsx
+++ cmd/clients/can-i-use-up-food/components/TitleBar/TitleBarView.tsx
@@ -6,11 +6,10 @@ interface Props {
 }
 
 export default function TitleBarView({title}: Props) {
-    const ctx = useContext(ProjectContext)
     return (
         <div className="w-screen h-12 bg-green-800 text-white shadow-md flex flex-row items-center justify-start px-4">
             <div className="text-2xl mr-4">Can I Use Up Food ?</div>
-            <div>{title}|{ctx.testName}|{ctx.testNumber}|{ctx.testChild.testChildName}</div>
+            <div>{title}</div>
         </div>
     )
 }
