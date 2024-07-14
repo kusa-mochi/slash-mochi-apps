e6e10b6a59406815925dc1bc2fc631fe6433930b
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-07-14 16:36:53 +0900

    commented out syntax error code.

--- cmd/clients/can-i-use-up-food/components/FoodListPane/FoodListPaneView.tsx
+++ cmd/clients/can-i-use-up-food/components/FoodListPane/FoodListPaneView.tsx
@@ -8,9 +8,9 @@ export default function FoodListPaneView() {
     return (
         <div className="h-full shadow-md bg-green-50">
             <div>foodlist-pane-view</div>
-            {foods.map(() =>
+            {/* {foods.map(() =>
                 <FoodListItem></FoodListItem>
-            )}
+            )} */}
         </div>
     )
 }
