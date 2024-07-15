c4fc3d7b8b53044c69148979509a64ce3c2fd40c
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-07-15 19:09:49 +0900

    read foods data from the context.

--- cmd/clients/can-i-use-up-food/components/FoodListPane/FoodListPane.tsx
+++ cmd/clients/can-i-use-up-food/components/FoodListPane/FoodListPane.tsx
@@ -1,13 +1,15 @@
 import { useContext } from "react";
 import FoodListPaneView from "./FoodListPaneView";
 import { ProjectSettingContext } from "@/context/ProjectSettingReducer";
+import { FoodsContext } from "@/context/FoodsReducer";
 
 export default function FoodListPane() {
     const { state: projectSettings } = useContext(ProjectSettingContext)
+    const { state: foods } = useContext(FoodsContext)
     return (
         <>
             <div>{projectSettings.projectName}</div>
-            <FoodListPaneView></FoodListPaneView>
+            <FoodListPaneView foods={foods}></FoodListPaneView>
         </>
     )
 }
--- cmd/clients/can-i-use-up-food/components/FoodListPane/FoodListPaneView.tsx
+++ cmd/clients/can-i-use-up-food/components/FoodListPane/FoodListPaneView.tsx
@@ -1,78 +1,14 @@
 import { AmountUnit } from "@/connect/can_i_use_up_food_pb"
+import { FoodType } from "@/types/Food"
 import FoodList from "@/ui-parts/FoodList/FoodList"
 import { useContext, useState } from "react"
 
-export default function FoodListPaneView() {
-    // TODO: only for debugging ->
-    const [foods, setFoods] = useState([
-        {
-            id: "aaaaaaaaaa000",
-            name: "鶏もも肉",
-            totalAmount: 2,
-            amountUnit: AmountUnit.PIECES,
-            limitDate: {
-                year: 2024,
-                month: 6,
-                date: 28,
-            },
-        },
-        {
-            id: "aaaaaaaaaa001",
-            name: "玉ねぎ",
-            totalAmount: 3,
-            amountUnit: AmountUnit.PIECES,
-            limitDate: {
-                year: 2024,
-                month: 7,
-                date: 10,
-            },
-        },
-        {
-            id: "aaaaaaaaaa002",
-            name: "卵",
-            totalAmount: 8,
-            amountUnit: AmountUnit.PIECES,
-            limitDate: {
-                year: 2024,
-                month: 7,
-                date: 4,
-            },
-        },
-        {
-            id: "aaaaaaaaaa003",
-            name: "にんじん",
-            totalAmount: 1,
-            amountUnit: AmountUnit.PIECES,
-            limitDate: {
-                year: 2024,
-                month: 6,
-                date: 30,
-            },
-        },
-        {
-            id: "aaaaaaaaaa004",
-            name: "コーンフレーク",
-            totalAmount: 500,
-            amountUnit: AmountUnit.GRAMS,
-            limitDate: {
-                year: 2024,
-                month: 7,
-                date: 10,
-            },
-        },
-        {
-            id: "aaaaaaaaaa005",
-            name: "みかんジュース",
-            totalAmount: 300,
-            amountUnit: AmountUnit.MILLI_LITER,
-            limitDate: {
-                year: 2024,
-                month: 7,
-                date: 10,
-            },
-        },
-    ])
-    // <- only for debugging
+type Props = {
+    foods: FoodType[]
+}
+
+export default function FoodListPaneView(props: Props) {
+    const foods = props.foods
     return (
         <div className="h-full shadow-md bg-green-50">
             <div>foodlist-pane-view</div>
