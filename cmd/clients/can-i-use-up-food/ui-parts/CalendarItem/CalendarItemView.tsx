f39a971a2f2a302c3b0ae71629f65c640e4cc4b9
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-08-12 18:20:26 +0900

    update bg color in calendar items.

--- cmd/clients/can-i-use-up-food/ui-parts/CalendarItem/CalendarItemView.tsx
+++ cmd/clients/can-i-use-up-food/ui-parts/CalendarItem/CalendarItemView.tsx
@@ -27,7 +27,7 @@ export default function CalendarItemView(props: Props) {
     }
 
     return (
-        <div className="w-full shadow-[0px_1px_2px_0px_rgba(0,0,0,0.45)] bg-green-200 rounded-full flex flex-row flex-nowrap justify-between items-center">
+        <div className="w-full shadow-[0px_1px_2px_0px_rgba(0,0,0,0.45)] bg-green-100 rounded-full flex flex-row flex-nowrap justify-between items-center">
             <div className="grow my-px ml-4 grid grid-cols-[1fr_40px]">
                 <div className="content-center">{props.foodName}</div>
                 <div className="text-sm text-gray-500 text-right content-center">{props.amount}&nbsp;{getAmountUnitString(props.unit)}</div>
