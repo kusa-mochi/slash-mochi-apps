134e430e1df96bceab53e4093e917c8d5d020a97
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-08-12 17:19:58 +0900

    add files to the list in the config file to be styled by tailwindcss.

--- cmd/clients/can-i-use-up-food/tailwind.config.ts
+++ cmd/clients/can-i-use-up-food/tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui-parts/**/*.{ts,tsx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
