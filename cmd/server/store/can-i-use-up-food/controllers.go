13a771ade26b6e38f6c8811c2148e2898e508418
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-08-15 16:09:16 +0900

    add controller func.

--- cmd/server/store/can-i-use-up-food/controllers.go
+++ cmd/server/store/can-i-use-up-food/controllers.go
@@ -17,6 +17,24 @@ func NewCanIUseUpFoodStoreControllers() *CanIUseUpFoodStoreControllers {
 	}
 }
 
+func (c *CanIUseUpFoodStoreControllers) LoadProjectData(req *server_common.GetSetRequest[ProjectData, can_i_use_up_foodv1.LoadProjectDataRequest]) {
+	projectId := req.DataToSet.GetProjectId()
+
+	// if a project is exist,
+	if projectData, ok := c.data.projects[projectId]; ok {
+		log.Printf("project %s found", projectId)
+
+		// prevent data r/w conflicts by copying data.
+		// Connect handler can only read copied data.
+		req.ResChan <- *projectData.Clone()
+	} else {
+		log.Printf("[E] project %s not found", projectId)
+
+		// return empty data
+		req.ResChan <- ProjectData{}
+	}
+}
+
 func (c *CanIUseUpFoodStoreControllers) NewProject(req *server_common.GetSetRequest[string, can_i_use_up_foodv1.NewProjectRequest]) {
 	startDate := req.DataToSet.GetStartDate()
 	startDatetime := time.Date(
@@ -52,22 +70,4 @@ func (c *CanIUseUpFoodStoreControllers) NewProject(req *server_common.GetSetRequ
 	)
 
 	req.ResChan <- projectId
-}
-
-func (c *CanIUseUpFoodStoreControllers) LoadProjectData(req *server_common.GetSetRequest[ProjectData, can_i_use_up_foodv1.LoadProjectDataRequest]) {
-	projectId := req.DataToSet.GetProjectId()
-
-	// if a project is exist,
-	if projectData, ok := c.data.projects[projectId]; ok {
-		log.Printf("project %s found", projectId)
-
-		// prevent data r/w conflicts by copying data.
-		// Connect handler can only read copied data.
-		req.ResChan <- *projectData.Clone()
-	} else {
-		log.Printf("[E] project %s not found", projectId)
-
-		// return empty data
-		req.ResChan <- ProjectData{}
-	}
 }
